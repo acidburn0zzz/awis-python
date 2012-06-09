92e74f09bda024097af0e3a88a929b5fcb9071ce
jwogrady <john@johnogrady.com>
2012-06-09 21:44:07 +0700

    added awis url generator

---
+++ awis-url-generator.py
@@ -0,0 +1,56 @@
+import datetime
+import urllib
+import hmac
+import hashlib
+import base64
+
+#Query Options
+Action = "UrlInfo"
+Url = "www.huffingtonpost.com/"
+ResponseGroup = "TrafficData"
+
+#Config Options
+PATH_TO_SECRET = 'c:\\users\\john\\aws-secret.txt' #Path to ANSI encoded text file with secret.
+AWSAccessKeyId = "AKIAJVPTTW3SPGN4OKQQ"
+
+#Don't edit below.
+f = open('c:\\users\\john\\aws-secret.txt') 
+secret = (f.read())
+SignatureVersion = "2"
+SignatureMethod = "HmacSHA256"
+ServiceHost = "awis.amazonaws.com"
+PATH = "/"
+
+def create_timestamp():
+    now = datetime.datetime.now()
+    timestamp = now.isoformat()
+    return timestamp
+
+def create_uri(params):
+    params = [(key, params[key])
+        for key in sorted(params.keys())]
+    return urllib.urlencode(params)
+
+def create_signature():
+    Uri = create_uri(params)
+    msg = "\n".join(["GET", ServiceHost, PATH, Uri])
+    hmac_signature = hmac.new(secret, msg, hashlib.sha256)
+    signature = base64.b64encode(hmac_signature.digest())
+    return urllib.quote(signature)
+
+params = {
+    'Action':Action,
+    'Url':Url,
+    'ResponseGroup':ResponseGroup,
+    'SignatureVersion':SignatureVersion,
+    'SignatureMethod':SignatureMethod,
+    'Timestamp': create_timestamp(),
+    'AWSAccessKeyId':AWSAccessKeyId,
+    }
+
+uri = create_uri(params)
+signature = create_signature()
+
+url = "http://%s/?%s&Signature=%s" % (ServiceHost, uri, signature)
+
+print url
